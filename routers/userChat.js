9b6df1a88ccda9e3bc261654624782524855b52a
Dương Hiển Thế <61902982+hacThe@users.noreply.github.com>
2022-12-24 16:54:24 +0700

    Update userChat.js

--- routers/userChat.js
+++ routers/userChat.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const userChatController = require("../controllers/userChatController")
+const userChatController = require("../controllers/UserChatController")
 
 router.post("/send-msg-to-admin", userChatController.sendMsgToAdmin)
 router.post("/send-msg-to-user", userChatController.sendMsgToUser)
